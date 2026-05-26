39aa32305c167544d9e0f1b7dc4a43336f58c62e
sandjonyves <sandjonyves@gmail.com>
2026-05-26 20:13:47 +0000

    Changes
    Co-authored-by: sandjonyves <117326116+sandjonyves@users.noreply.github.com>

--- src/presentation/components/client/ClientLayout.tsx
+++ src/presentation/components/client/ClientLayout.tsx
@@ -1,12 +1,16 @@
 import { ClientHeader } from './ClientHeader';
 import { ClientFooter } from './ClientFooter';
+import { WhatsAppFab } from './WhatsAppFab';
+import { useTheme } from '../../../application/hooks/useTheme';
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
+  useTheme(); // initialise le thème (système / persisté)
   return (
     <div className="min-h-screen flex flex-col bg-background">
       <ClientHeader />
       <main className="flex-1">{children}</main>
       <ClientFooter />
+      <WhatsAppFab />
     </div>
   );
 }
